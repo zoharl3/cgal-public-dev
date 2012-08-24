1eee62f76d1c9483ac0f7104547249fae718ad6d
Ílker Yaz <ilkeryaz@gmail.com>
2012-08-24 19:33:41 +0000

    Now, returned segment-ids are ordered by average sdf value of the segment (facets that construct that segment).

--- Surface_mesh_segmentation/include/CGAL/internal/Surface_mesh_segmentation/Alpha_expansion_graph_cut.h
+++ Surface_mesh_segmentation/include/CGAL/internal/Surface_mesh_segmentation/Alpha_expansion_graph_cut.h
@@ -73,7 +73,7 @@ private:
     
     typedef boost::adjacency_list<boost::vecS, boost::listS, boost::directedS,
         // 4 vertex properties
-        boost::property<boost::vertex_index_t, int,
+        boost::property<boost::vertex_index_t, std::size_t,
         boost::property<boost::vertex_color_t, boost::default_color_type,
         boost::property<boost::vertex_distance_t, double,
         boost::property<boost::vertex_predecessor_t, Adjacency_list_traits::edge_descriptor > > > >,
@@ -116,7 +116,7 @@ private:
     
 public:
 
-double operator()(std::vector<std::pair<int, int> >& edges,
+double operator()(const std::vector<std::pair<int, int> >& edges,
                   const std::vector<double>& edge_weights,
                   const std::vector<std::vector<double> >& probability_matrix,
                   std::vector<int>& labels) const
@@ -216,7 +216,7 @@ private:
     
     typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
         // 4 vertex properties
-        boost::property<boost::vertex_index_t, int,
+        boost::property<boost::vertex_index_t, std::size_t,
         boost::property<boost::vertex_color_t, boost::default_color_type,
         boost::property<boost::vertex_distance_t, double,
         boost::property<boost::vertex_predecessor_t, Adjacency_list_traits::edge_descriptor > > > >,
@@ -256,7 +256,7 @@ private:
     
 public:
 
-double operator()(std::vector<std::pair<int, int> >& edges,
+double operator()(const std::vector<std::pair<int, int> >& edges,
                   const std::vector<double>& edge_weights,
                   const std::vector<std::vector<double> >& probability_matrix,
                   std::vector<int>& labels) const
@@ -342,7 +342,7 @@ double operator()(std::vector<std::pair<int, int> >& edges,
 class Alpha_expansion_graph_cut_boykov_kolmogorov
 {
 public:
-double operator()(std::vector<std::pair<int, int> >& edges,
+double operator()(const std::vector<std::pair<int, int> >& edges,
                   const std::vector<double>& edge_weights,
                   const std::vector<std::vector<double> >& probability_matrix,
                   std::vector<int>& labels) const
